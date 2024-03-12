b297fa28ee41ead6cf24049e4db1ad2dd145065b
Stresser <hartmann514@protonmail.com>
2024-03-12 10:45:27 +0200

    Updateeeeee! Yay!

--- c-comp.cxx
+++ c-comp.cxx
@@ -1,33 +1,68 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string>
+#include <utility>
 
-//securitee? readabilitee? who da fuck needs that?
+/* Securitee? Readabilitee? Who da duck needs that? */
 
-std::string ext_rm(char* val)
+bool s_known = false;
+
+std::pair<char*, char*> parser(int argc, char** argv)
 {
-    std::string input(val);
-    size_t dot = input.find_last_of('.');
-    if (dot!=std::string::npos)
+    /* 64 kilobytes ought to be enough for anybody */
+    char lnk[65536] = "/MACHINE:X64 ";
+    char src[65536]{};
+    for (int i = argc - 1; i > 0; i--)
     {
-        input.erase(dot,sizeof(long long));
+        std::string arg(argv[i]);
+        if (strcmp(arg.substr(0, 2).c_str(), "--") == 0)
+        {
+            goto PARAM;
+        }
+        {
+            arg.insert(0, "\"");
+            strcat(src, (arg += "\" ").c_str());
+            continue;
+        }
+PARAM: /* summoning the YandereDev */
+        if (strcmp(arg.substr(2).c_str(), "dll") == 0)
+        {
+            strcat(lnk, "/DLL "); continue;
+        }
+        if (strcmp(arg.substr(2, 1).c_str(), "s") == 0)
+        {
+            strcat(lnk, "/SUBSYSTEM:");
+            if (strcmp(arg.substr(4).c_str(), "con") == 0)
+            {
+                strcat(lnk, "CONSOLE ");
+            } else
+            if (strcmp(arg.substr(4).c_str(), "win32") == 0)
+            {
+                strcat(lnk, "WINDOWS ");
+            } else
+            {
+                strcat(lnk, "CONSOLE ");
+            } s_known = true; continue;
+        }
     }
-    return input;
+    if (!s_known) strcat(lnk, "/SUBSYSTEM:CONSOLE ");
+    return std::make_pair(lnk, src);
 }
-
 int main(int argc, char** argv)
 {
-	if (argc < 2) { puts("How do i compile the nothing?"); goto ERROR; }
-	char compyil[0x100];
-	char deleet[sizeof(compyil)>>2];
-	sprintf(compyil, "cl /EHsc /W0 /std:c++17 /O2 /I\"D:\\Windows Kits\\10\\Include\\10.0.17763.0\\ucrt\" /I\"D:\\DIMA\\ALL\\SDK\\Visual Studio\\IDE\\VC\\Tools\\MSVC\\14.16.27023\\include\" /Fo:\"%s\".~temp \"%s\"", ext_rm(argv[1]).c_str(), argv[1]);
-	sprintf(deleet, "del \"%s.~temp\"", ext_rm(argv[1]).c_str());
-    std::system("@echo off");
-    std::system("set PATH=\%PATH\%;D:\\DIMA\\ALL\\SDK\\Visual Studio\\IDE\\VC\\Tools\\MSVC\\14.16.27023\\bin\\Hostx64\\x64");
-    std::system("set LIB=\%LIB\%;D:\\DIMA\\ALL\\SDK\\Visual Studio\\IDE\VC\\Tools\\MSVC\\14.16.27023\\lib\x64;D:\\Windows Kits\\10\\Lib\\10.0.17763.0\\um\\x64;D:\\Windows Kits\\10\\Lib\\10.0.17763.0\\ucrt\\x64");
-    std::system(compyil);
-    std::system(deleet);
-
+	if (argc < 2) { puts("Usage: c-comp (sources <one with main() must be passed last>) [--dll] [--s:con/win32]\0"); goto ERROR; }
+    system("@echo off");
+    {
+        char lib[]  = "/LIBPATH:\"D:\\DIMA\\ALL\\SDK\\Visual Studio\\Build Tools\\VC\\Tools\\MSVC\\14.29.30133\\lib\\x64\" /LIBPATH:\"D:\\DIMA\\ALL\\SDK\\Visual Studio\\Windows Kits\\10\\Lib\\10.0.19041.0\\ucrt\\x64\" /LIBPATH:\"D:\\DIMA\\ALL\\SDK\\Visual Studio\\Windows Kits\\10\\Lib\\10.0.19041.0\\um\\x64\" /LIBPATH:\"D:\\DIMA\\ALL\\SDK\\_Libraries\\SDL2\\lib\\x64\" /LIBPATH:\"D:\\DIMA\\ALL\\SDK\\_Libraries\\bass\\c\"";
+        char incl[] = "/I\"D:\\DIMA\\ALL\\SDK\\Visual Studio\\Build Tools\\VC\\Tools\\MSVC\\14.29.30133\\include\" /I\"D:\\DIMA\\ALL\\SDK\\Visual Studio\\Windows Kits\\10\\Include\\10.0.19041.0\\ucrt\" /I\"D:\\DIMA\\ALL\\SDK\\Visual Studio\\Windows Kits\\10\\Include\\10.0.19041.0\\um\" /I\"D:\\DIMA\\ALL\\SDK\\_Libraries\\SDL2\\include\" /I\"D:\\DIMA\\ALL\\SDK\\_Libraries\\bass\\c\"";
+        char compyil[65536];
+        const char* deleet = "del *.~temp";
+        auto [lnk, src] = parser(argc, argv);
+        sprintf(compyil, "cl /nologo /std:c++latest /EHsc /Oy /O2 /Gd /J %s %s/TP /Fo\".~temp\" /link %s %s/out:\"output.exe\"", incl, src, lib, lnk);
+        printf("%s\n", compyil);
+        system(compyil);
+        system(deleet);
+    }
 SUCCESS:
     return 0;
 ERROR:
