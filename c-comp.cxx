63a22083dbad74c882e5eb3bf40569269e88982e
Stresser <hartmann514@protonmail.com>
2024-03-16 15:58:39 +0200

    Softy

--- c-comp.cxx
+++ c-comp.cxx
@@ -1,42 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string>
+#include <tuple>
 #include <utility>
 
 /* Securitee? Readabilitee? Who da duck needs that? */
 
+constexpr unsigned short kb_4 = 4096;
 bool s_known = false;
 
-std::pair<char*, char*> parser(int argc, char** argv)
+char* args(int argc, char** argv);
+std::tuple<char*, char*, char*> fetch();
+
+int main(int argc, char** argv)
 {
+    system("@echo off");
     /* 64 kilobytes ought to be enough for anybody */
+    char compyil[65536];
+    const char* deleet = "del *.obj";
+    char* lnk = args(argc, argv);
+    auto [incl, lib, src] = fetch();
+    sprintf(compyil, "cl /nologo /std:c++latest /EHsc /O2 /Gd /J %s%s/TP /link %s%s\n\n\0", incl, src, lib, lnk);
+    printf("%s", compyil);
+    system(compyil);
+    system(deleet);
+    return 0;
+}
+
+char* args(int argc, char** argv)
+{
     char lnk[65536] = "/MACHINE:X64 ";
-    char src[65536]{};
     for (int i = argc - 1; i > 0; i--)
     {
+        /* summoning the YandereDev */
         std::string arg(argv[i]);
-        if (strcmp(arg.substr(0, 2).c_str(), "--") == 0)
-        {
-            goto PARAM;
-        }
-        {
-            arg.insert(0, "\"");
-            strcat(src, (arg += "\" ").c_str());
-            continue;
-        }
-PARAM: /* summoning the YandereDev */
-        if (strcmp(arg.substr(2).c_str(), "dll") == 0)
+        if (strcmp(arg.c_str(), "dll") == 0)
         {
             strcat(lnk, "/DLL "); continue;
         }
-        if (strcmp(arg.substr(2, 1).c_str(), "s") == 0)
+        if (strcmp(arg.c_str(), "s") == 0)
         {
             strcat(lnk, "/SUBSYSTEM:");
-            if (strcmp(arg.substr(4).c_str(), "con") == 0)
+            if (strcmp(arg.substr(2).c_str(), "con") == 0)
             {
                 strcat(lnk, "CONSOLE ");
             } else
-            if (strcmp(arg.substr(4).c_str(), "win32") == 0)
+            if (strcmp(arg.substr(2).c_str(), "win32") == 0)
             {
                 strcat(lnk, "WINDOWS ");
             } else
@@ -46,25 +55,23 @@ PARAM: /* summoning the YandereDev */
         }
     }
     if (!s_known) strcat(lnk, "/SUBSYSTEM:CONSOLE ");
-    return std::make_pair(lnk, src);
+    return lnk;
 }
-int main(int argc, char** argv)
+
+std::tuple<char*, char*, char*> fetch()
 {
-	if (argc < 2) { puts("Usage: c-comp (sources <one with main() must be passed last>) [--dll] [--s:con/win32]\0"); goto ERROR; }
-    system("@echo off");
+    char*(*istr)(_iobuf*, const char*) = <::> (_iobuf* stream, const char* key)
     {
-        char lib[]  = "/LIBPATH:\"D:\\DIMA\\ALL\\SDK\\Visual Studio\\Build Tools\\VC\\Tools\\MSVC\\14.29.30133\\lib\\x64\" /LIBPATH:\"D:\\DIMA\\ALL\\SDK\\Visual Studio\\Windows Kits\\10\\Lib\\10.0.19041.0\\ucrt\\x64\" /LIBPATH:\"D:\\DIMA\\ALL\\SDK\\Visual Studio\\Windows Kits\\10\\Lib\\10.0.19041.0\\um\\x64\" /LIBPATH:\"D:\\DIMA\\ALL\\SDK\\_Libraries\\SDL2\\lib\\x64\" /LIBPATH:\"D:\\DIMA\\ALL\\SDK\\_Libraries\\bass\\c\"";
-        char incl[] = "/I\"D:\\DIMA\\ALL\\SDK\\Visual Studio\\Build Tools\\VC\\Tools\\MSVC\\14.29.30133\\include\" /I\"D:\\DIMA\\ALL\\SDK\\Visual Studio\\Windows Kits\\10\\Include\\10.0.19041.0\\ucrt\" /I\"D:\\DIMA\\ALL\\SDK\\Visual Studio\\Windows Kits\\10\\Include\\10.0.19041.0\\um\" /I\"D:\\DIMA\\ALL\\SDK\\_Libraries\\SDL2\\include\" /I\"D:\\DIMA\\ALL\\SDK\\_Libraries\\bass\\c\"";
-        char compyil[65536];
-        const char* deleet = "del *.~temp";
-        auto [lnk, src] = parser(argc, argv);
-        sprintf(compyil, "cl /nologo /std:c++latest /EHsc /Oy /O2 /Gd /J %s %s/TP /Fo\".~temp\" /link %s %s/out:\"output.exe\"", incl, src, lib, lnk);
-        printf("%s\n", compyil);
-        system(compyil);
-        system(deleet);
-    }
-SUCCESS:
-    return 0;
-ERROR:
-	return -1;
+        char buf[kb_4]; char* str = new char[kb_4]{};
+        while (fgets(buf, sizeof buf, stream) != NULL)
+        {
+            buf[strcspn(buf, "\n")] = 0;
+            strcat(str, key);
+            strcat(str, buf);
+            strcat(str, "\" ");
+        }
+        fclose(stream);
+        return str;
+    };
+    return std::make_tuple(istr(fopen("incl.txt", "r"), "/I\""), istr(fopen("lib.txt", "r"), "/LIBPATH:\""), istr(fopen("src.txt", "r"), "\""));
 }
